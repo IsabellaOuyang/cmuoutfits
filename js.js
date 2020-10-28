f6244e3208175ac4636d5b11a8b5cd6dd8c12e3c
IsabellaOuyang <yushano@andrew.cmu.edu>
2020-10-28 15:12:26 -0400

    update

---
+++ js.js
@@ -0,0 +1,58 @@
+
+$('.scrollTo').click(function(){
+
+var svg = document.getElementsByTagName("svg")[0];
+svg.setAttribute("viewBox","0 800 375 17107");
+
+
+});
+
+
+function changeAnswerCFA(){
+
+
+	$("#rightCFA").removeAttr('id');
+	$('#rightCFA').fadeIn();
+
+
+		}
+
+
+function changewrongAnswerCFA() {
+
+	$("#wrongCFA").removeAttr('id');
+	$('#wrongCFA').fadeIn();;
+		}
+
+
+function changeAnswerCIT() {
+
+	$("#rightCIT").removeAttr('id');
+	$('#rightCIT').fadeIn();;
+
+		}
+
+
+function changewrongAnswerCIT() {
+
+	$("#wrongCIT").removeAttr('id');
+	$('#wrongCIT').fadeIn();;
+
+		}
+
+function changeAnswerDIET() {
+
+	$("#rightDIET").removeAttr('id');
+	$('#rightDIET').fadeIn();
+
+		}
+
+
+function changewrongAnswerDIET() {
+
+	$("#wrongDIET").removeAttr('id');
+	$('#wrongDIET').fadeIn();;
+
+		}
+
+
